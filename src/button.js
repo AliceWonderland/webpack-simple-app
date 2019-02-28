690c371516e2292ea7a25c58f8bd851d52d5fc64
Alice Chuang <alice.chuang@cbsinteractive.com>
2019-02-28 15:36:53 -0500

    adding button

--- src/button.js
+++ src/button.js
@@ -1,5 +1,10 @@
 // take button label (str), return button ele
 // commonjs example
+/**
+ * 
+ * @param {string} buttonLabel 
+ * @returns {Element}
+ */
 const makeButton = (buttonLabel) => {
     return `Button: ${buttonLabel}`;
 };
